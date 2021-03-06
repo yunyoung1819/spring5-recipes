a0a93604a8e0a2012594a49e6ee9dd015269319a
yunyoung1819 <yunyoung1819@hanmail.net>
2021-03-06 20:51:30 +0900

    3장 스프링 MVC 스터디

---
+++ ch03/recipe_3_1/src/main/java/com/young/spring/court/config/CourtConfiguration.java
@@ -0,0 +1,9 @@
+package com.young.spring.court.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.young.spring.court")
+public class CourtConfiguration {
+}
---
+++ ch03/recipe_3_1/src/main/java/com/young/spring/court/web/CourtServletContainerInitializer.java
@@ -0,0 +1,27 @@
+package com.young.spring.court.web;
+
+import com.young.spring.court.config.CourtConfiguration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import java.util.Set;
+
+public class CourtServletContainerInitializer implements ServletContainerInitializer {
+
+
+    @Override
+    public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
+        AnnotationConfigWebApplicationContext applicationContext =
+                new AnnotationConfigWebApplicationContext();
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(applicationContext);
+        ServletRegistration.Dynamic courtRegistration =
+                ctx.addServlet("court", dispatcherServlet);
+        courtRegistration.setLoadOnStartup(1);
+        courtRegistration.addMapping("/");
+    }
+}
